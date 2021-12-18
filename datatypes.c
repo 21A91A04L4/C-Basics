bf79224ce55bea38f56f56c035aee6a8266ab202
Sudhakommireddy <96324729+Sudhakommireddy@users.noreply.github.com>
2021-12-18 15:25:23 +0530

    Add files via upload

---
+++ datatypes.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+void main()
+{
+	  int age;
+	  float perc;
+	  char initial;
+	  
+	  age=17;
+	  printf("Age is:%d", age);
+	  perc=98.66;
+	  printf("\n percentage is:%f", perc);
+	  initial='k';
+	  printf("\n initial is:%c", initial);
+}
